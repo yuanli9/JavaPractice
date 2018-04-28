c011b6282dadcbff7e7bab5c93279463825575b2
yuanli9 <17612180840@163.com>
2018-04-28 18:13:40 +0800

    ""

--- src/main/java/cn/eli/map/KeyTest.java
+++ src/main/java/cn/eli/map/KeyTest.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class ketTest {
+public class KeyTest {
     public static void main(String[] args) {
 
         Map<String ,Integer> map = new HashMap<>();
